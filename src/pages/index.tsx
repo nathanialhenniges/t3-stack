0d72e16628ceebed3889e6482534006fadf72ce1
Nathanial Henniges <nathanial.henniges@mrdemonwolf.com>
2023-09-02 21:54:02 -0500

    Added "router"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import Head from "next/head";
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 
+import { api } from "~/utils/api";
 
 export default function Home() {
   const user = useUser();
+  
+  const { data } = api.posts.getAll.useQuery()
 
   return (
     <>
@@ -13,8 +16,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-      {!user.isSignedIn && <SignInButton />}
+       <div>
+         {!user.isSignedIn && <SignInButton />}
       {!!user.isSignedIn && <SignOutButton />}
+      </div>
+      <div>
+        {data?.map((post) => (<div key={post.id}>{post.content}</div>))}
+      </div>
       </main>
     </>
   );
