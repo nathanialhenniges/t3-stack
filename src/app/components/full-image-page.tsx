046ed1a74770695fa06f6b57c364b9fdade008e6
Nathanial Henniges <nathanial.henniges@mrdemonwolf.com>
2024-11-12 23:33:49 -0600

    More clean up on modal

--- src/app/components/full-image-page.tsx
+++ src/app/components/full-image-page.tsx
@@ -2,5 +2,15 @@ import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
-  return <img src={image.url} className="h-96" />;
+  // eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text
+  return (
+    <div className="0 flex h-full w-full min-w-0">
+      <div className="flex flex-shrink items-center justify-center">
+        <img src={image.url} className="flex-shrink object-contain" />
+      </div>
+      <div className="flex w-48 flex-shrink-0 flex-col border-l">
+        <div className="text-xl font-bold">{image.name}</div>
+      </div>
+    </div>
+  );
 }
