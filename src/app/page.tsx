f55530158d200f4af489322873d1203844d4c80f
Nathanial Henniges <nathanial.henniges@mrdemonwolf.com>
2024-11-12 23:41:20 -0600

    move to grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default async function HomePage() {
     const images = await getMyImages();
     return (
       <div className="flex flex-wrap justify-center gap-4 p-4">
-        {images.map((image) => (
+        {images.reverse().map((image) => (
           <div key={image.id} className="flex h-48 w-48 flex-col">
             <Link href={`/img/${image.id}`}>
               <Image
